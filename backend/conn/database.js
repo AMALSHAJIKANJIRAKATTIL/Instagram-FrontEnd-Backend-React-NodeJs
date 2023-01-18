f812eab727fefa7db947c53940c5e1e14571f7b3
AMALSHAJIKANJIRAKATTIL <amalshajikmofficial@gmail.com>
2023-01-18 15:39:31 +0530

    Databse update

--- backend/conn/database.js
+++ backend/conn/database.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 
 async function getconnection(){
     mongoose.set('strictQuery', true);
-    await (await (await mongoose.connect('mongodb+srv://amalshaji:#Amal2001@cluster0.fguegcf.mongodb.net/instaclone?retryWrites=true&w=majority')));
+    await (await (await mongoose.connect('mongodb+srv://amalshaji:amal2001@cluster0.fguegcf.mongodb.net/instaclone?retryWrites=true&w=majority')));
 }
 
 module.exports = getconnection;
